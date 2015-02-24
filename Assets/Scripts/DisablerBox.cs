2993f53e4f45fbddf449e49aad60d441cb1a2903
Geordie Powers <geordie.powers@gmail.com>
2015-02-24 17:41:41 -0500

    Began adding "gameobject deactivator box"

---
+++ Assets/Scripts/DisablerBox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisablerBox : MonoBehaviour {
+    void OnTriggerEnter(Collider otherCollider) {
+        otherCollider.gameObject.SetActive(false);
+    }
+
+    void OnCollisionExit(Collision collisionInfo) {
+        collisionInfo.gameObject.SetActive(false);
+    }
+}
